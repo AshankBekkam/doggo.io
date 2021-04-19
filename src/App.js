9a0413cb768b446a1793e3f79f9bd747bb0ea9ce
ashank <ashankbekkam@gmail.com>
2021-04-19 09:38:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Link, Switch, BrowserRouter as Router } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Homepage } from './pages/Homepage';
+import  {InfoPage}  from './pages/InfoPage.jsx';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      dogs: [],
+      
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.thedogapi.com/v1/breeds')
+      .then(response => response.json())
+      .then(dog => this.setState({ dogs: dog }));
+  }
+
+
+  render() {
+    const { dogs } = this.state;
+    const allDogs = dogs.filter(dog =>
+      dog.name
+    );
+    const filteredDogs = allDogs.slice(0,20)
+    return (
+      <Router>
+        <Switch>
+          <Route path = "/" exact component = {()=><Homepage dogs = {filteredDogs}/>} />
+          <Route path = '/infopage' exact component = {InfoPage}/>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
